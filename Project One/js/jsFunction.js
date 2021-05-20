d544ef8041917796d7c214f9fba82905de39ca5a
Md. Murad Ali <53254539+Md-Ali63@users.noreply.github.com>
2021-05-21 01:04:34 +0600

    Add files via upload

---
+++ Project One/js/jsFunction.js
@@ -0,0 +1,14 @@
+function Close(){
+  x = document.getElementById("demo");
+  //change style attribute 
+  //x.style.display = "none";
+  // change class name
+  x.className="closed";
+}
+function Open(){
+  x  = document.getElementById("demo");
+  //change style attribute
+  //x.style.display = "block";
+  //change class name
+  x.className = "open";
+}
